0d4f4d6bb691c5b78916ab2df4a6793b3f9aebb8
ErvinTyx1010 <yxteoh-wp22@student.edu.my>
2025-04-05 23:15:30 +0800

    Start implementation for job seaching

--- src/main/java/control/InterviewScheduler.java
+++ src/main/java/control/InterviewScheduler.java
@@ -1,5 +1,10 @@
 package control;
 
-public class InterviewScheduler {
+import adt.ArrayList;
+import entity.Match;
+public abstract class InterviewScheduler {
     
+    public abstract void scheduleInterviews(ArrayList<Match> matches);
+
+    public abstract void filterApplicantProfiles();
 }
