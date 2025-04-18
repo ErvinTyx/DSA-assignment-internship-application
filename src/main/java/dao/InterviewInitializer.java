443f8b4738b29511ce69b5d3e1ba3cc8698bb68a
ErvinTyx1010 <yxteoh-wp22@student.edu.my>
2025-04-18 12:12:03 +0800

    Start initialize dao for matching and interview

---
+++ src/main/java/dao/InterviewInitializer.java
@@ -0,0 +1,9 @@
+package dao;
+
+import control.*;
+
+public class InterviewInitializer {
+    public static void initialize(InterviewSchedulerManager interviews, MatchingEngine matchingEngine, CompanyManager companyManager, JobManager jobManager, ApplicantManager applicantManager) {
+        
+    }
+}
---
+++ src/main/java/dao/MatchInitializer.java
@@ -0,0 +1,13 @@
+package dao;
+
+import adt.ListInterface;
+import control.ApplicantManager;
+import control.CompanyManager;
+import control.JobManager;
+import control.MatchingEngine;
+public class MatchInitializer {
+    public static void matchStudents(MatchingEngine matchingEngine, JobManager jobManager, CompanyManager companyManager, ApplicantManager applicantManager) {
+        // TODO: Enter new match data
+        
+    }
+}
