bc39e612ba4906f60c15472120334a08ecb4779a
ErvinTyx1010 <yxteoh-wp22@student.edu.my>
2025-05-03 23:16:13 +0800

    Fixed warnings Company Manager

--- src/main/java/control/CompanyManager.java
+++ src/main/java/control/CompanyManager.java
@@ -11,13 +11,13 @@ import dao.CompanyDAO;
 public class CompanyManager {
 
     private ListInterface<Company> companyList = new ArrayList<>();
-    private JobManager jobManager = new JobManager(new ArrayList<>());
-    private CompanyUI companyUI = new CompanyUI();
-    private CompanyDAO companyDAO = new CompanyDAO();
-    private InterviewSchedulerManager interviewSchedulerManager = new InterviewSchedulerManager(null);
+    private final JobManager jobManager = new JobManager(new ArrayList<>());
+    private final CompanyUI companyUI = new CompanyUI();
+    private final CompanyDAO companyDAO = new CompanyDAO();
+    private final InterviewSchedulerManager interviewSchedulerManager = new InterviewSchedulerManager(null);
     private int index = -1;
 
-    public CompanyManager(){
+    public CompanyManager() {
         companyList = companyDAO.retrieveFromFile();
     }
 
@@ -26,15 +26,14 @@ public class CompanyManager {
         while (flag) {
             index = companyUI.getMenuChoiceLogin();
             switch (index) {
-                case 1:
+                case 1 ->
                     login();
-                    break;
-                case 2:
+                case 2 ->
                     signUp();
-                    break;
-                case 3:
+                case 3 -> {
                     flag = false;
-                    break;
+                    MessageUI.displayLogOutMessage();
+                }
             }
         }
         companyDAO.saveToFile(companyList);
@@ -61,21 +60,23 @@ public class CompanyManager {
                         // update
                         int updateChoice = companyUI.getUpdateMenuChoice();
                         switch (updateChoice) {
-                            case 1:
+                            case 1 -> {
                                 String name = companyUI.inputCompanyName();
                                 companyList.get(index).setName(name);
-                                break;
-                            case 2:
+                            }
+                            case 2 -> {
                                 String location = companyUI.inputCompanyLocation();
                                 companyList.get(index).setLocation(location);
-                                break;
-                            case 3:
-                                break;
-                            default:
+                            }
+                            case 3 -> {
+
+                            }
+                            default ->
                                 MessageUI.displayInvalidChoiceMessage();
                         }
                         companyUI.listCompanyInfo(displayCompanyInfo(companyList.get(index)));
                         break;
+
                     case 4:
                         jobManager.runJobPosting(companyList.get(index).getJobPostings());
                         ListInterface<JobPosting> jobPostings = jobManager.getJobPostings();
@@ -104,7 +105,7 @@ public class CompanyManager {
                         MessageUI.displayInvalidChoiceMessage();
                 }
             } while (choice != 6);
-        }else{
+        } else {
             MessageUI.displayCompanyInfoNotFoundMessage();
         }
     }
@@ -132,25 +133,25 @@ public class CompanyManager {
     public ListInterface<JobPosting> searchJobs(String jobTitle, int weighting) {
         ListInterface<JobPosting> result = new ArrayList<>();
         for (int i = 0; i < companyList.size(); i++) {
-           
+
             // Get job postings for this company
             ListInterface<JobPosting> companyJobs = companyList.get(i).getJobPostings();
-            
+
             // Check if jobManager is properly initialized
             if (jobManager == null) {
                 System.out.println("ERROR: jobManager is null");
                 continue;
             }
-            
+
             // Search for matching jobs in this company
             ListInterface<JobPosting> foundJob = jobManager.searchJobs(jobTitle, weighting, companyJobs);
-            
+
             // Add all found jobs to the result list
             for (int j = 0; j < foundJob.size(); j++) {
                 result.add(foundJob.get(j));
             }
         }
-        
+
         System.out.println("Total matching jobs found across all companies: " + result.size());
         return result;
     }
